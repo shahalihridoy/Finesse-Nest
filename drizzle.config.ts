e14c94e2a4742657ad6f444be11915a98c9643a7
Md Shah Ali <shahalihridoy@gmail.com>
2025-09-11 12:17:33 -0400

    Update packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,15 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/database/schema/*",
   out: "./src/database/migrations",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DB_URL
-    // host: process.env.DB_HOST || "localhost",
-    // port: parseInt(process.env.DB_PORT || "5432"),
-    // user: process.env.DB_USER || "postgres",
-    // password: process.env.DB_PASSWORD || "",
-    // database: process.env.DB_DATABASE || "finesse_db",
-    // ssl: true
+    url: process.env.DB_URL
   },
   verbose: true,
   strict: true
